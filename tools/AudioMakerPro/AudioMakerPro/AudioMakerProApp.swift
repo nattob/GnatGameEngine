575e32246bb0857864b0da1aa83cb570ba2fbffc
Family Tater <familytater@Familys-MacBook-Air.local>
2023-07-23 20:30:03 -0700

    Create AudioMakerPro

---
+++ tools/AudioMakerPro/AudioMakerPro/AudioMakerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMakerProApp.swift
+//  AudioMakerPro
+//
+//  Created by Family Tater on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioMakerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
