bb572f20f6e7ee8c3718a17b6eb3baa6088a6927
Family Tater <familytater@Familys-MacBook-Air.local>
2023-07-23 20:15:29 -0700

    AudioPP blank window retry

---
+++ tools/AudioMakerPro/AudioMakerPro/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AudioMakerPro
+//
+//  Created by Family Tater on 7/23/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ tools/AudioMakerPro/AudioMakerPro/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AudioMakerPro
+//
+//  Created by Family Tater on 7/23/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
